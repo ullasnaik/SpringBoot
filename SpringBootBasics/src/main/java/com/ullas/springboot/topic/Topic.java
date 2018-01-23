bdeba1dd3e6031c9ab95a3f4b49cdfc2fd81b833
Ullas Naik <naik.ullas@ymail.com>
2018-01-24 02:43:20 +0530

    SBB

---
+++ SpringBootBasics/src/main/java/com/ullas/springboot/topic/Topic.java
@@ -0,0 +1,45 @@
+package com.ullas.springboot.topic;
+
+public class Topic {
+
+	private String name;
+	private String id;
+	private String description;
+ 
+	public Topic() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Topic(String name, String id, String description) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
